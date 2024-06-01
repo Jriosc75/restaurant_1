39f076f10c1c922a813664035e3644f29c0db0df
APUSKIPAY2014 <soporteapuskipay@gmail.com>
2024-05-31 22:19:07 -0500

    feat: button-landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,12 @@
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 export default function Landing() {
+    const navigate = useNavigate()
     return (
         <Box border={'1px solid red'}>
             <h1>LANDING</h1>
+            <Button onClick={() => navigate('/131231/menu')}>fsfs</Button>
         </Box>
     )
 }
